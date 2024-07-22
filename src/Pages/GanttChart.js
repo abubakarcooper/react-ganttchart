34a499ca090221968108e228976902504239bb4c
abubakarwaqas <abu@cooperdevelopment.com>
2024-07-23 00:25:46 +0500

    build issue resolv

--- src/Pages/GanttChart.js
+++ src/Pages/GanttChart.js
@@ -1,8 +1,8 @@
-// import Gantt2 from "../component/Gantt2.js"
+import Gantt2 from "../components/Gantt2.js"
 const GanttChartPage = () => {
     return <div className='gantt-chart'>
         <h1>Helloo </h1>
-        {/* <Gantt2 /> */}
+        <Gantt2 />
     </div>
 }
 
